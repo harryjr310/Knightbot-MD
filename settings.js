c897f5553d5e3ee76453fbba4beb2f556ecf9e87
harryjr310 <harryjr310@gmail.com>
2025-03-17 16:57:11 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'HARRYJR', // Your name
+  ownerNumber: '+27 78 317 1568', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
